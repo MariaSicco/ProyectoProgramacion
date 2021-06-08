adfe3bd2d26c946e5918f23b917db759da7ef0fa
FranciscoRolon <frolon@udesa.edu.ar>
2021-06-08 14:36:12 -0300

    js

--- js/master.js
+++ js/master.js
@@ -1,5 +1,8 @@
+//Fetch Canciones del momento
 let musica = document.querySelector('.musica');
+
 fetch ('https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/302127')
+
 .then(function(response){
     return response.json()
 })
@@ -10,3 +13,33 @@ fetch ('https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/302127'
     console.log(error);
 })
 //Hay que arreglarlo
+
+//Fetch Top Albums
+let musica = document.querySelector('.musica');
+
+fetch ('')
+
+.then(function(response){
+    return response.json()
+})
+.then(function(dataMusica){
+    console.log(dataMusica);
+})
+.catch(function(error){
+    console.log(error);
+})
+
+//Fetch Artistas
+let musica = document.querySelector('.musica');
+
+fetch ('')
+
+.then(function(response){
+    return response.json()
+})
+.then(function(dataMusica){
+    console.log(dataMusica);
+})
+.catch(function(error){
+    console.log(error);
+})
