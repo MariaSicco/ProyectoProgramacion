9017e7aec60a1f4153e93b9a95d9ab25efbaef53
abriniell <69683991+abriniell@users.noreply.github.com>
2021-06-22 18:25:20 -0300

    agregar a playlist

--- js/detalle-cancion.js
+++ js/detalle-cancion.js
@@ -2,6 +2,7 @@ let fotoCancion = document.querySelector('.fotoCancion')
 let nombreCancion = document.querySelector('.nombreCancion')
 let nombreArtista = document.querySelector('.nombreArtista')
 let nombreAlbum = document.querySelector('.nombreAlbum')
+let playlist = document.querySelector('.playlist')
 let video  = document.querySelector('.video')
 
 let objetoId = new URLSearchParams(location.search)
@@ -18,6 +19,7 @@ fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${id}`)
     fotoCancion.innerHTML += `<article class="fotos"> <img src="${Datacancion.album.cover_big}">  </article>`
     nombreArtista.innerHTML +=`${Datacancion.artist.name}`
     nombreAlbum.innerHTML += `${Datacancion.album.title}`
+    playlist.innerHTML += `<a href="#"> Agregar a mi playlist </a>`
     video.innerHTML += `<iframe title="deezer-widget" src="https://widget.deezer.com/widget/dark/track/${Datacancion.id}" width="100%" height="300" frameborder="0" allowtransparency="true" allow="encrypted-media; clipboard-write"></iframe>`  
 })
 .catch(error=>{
@@ -29,3 +31,26 @@ nombreCancion.classList.add('nombreCancion')
 nombreArtista.classList.add('nombreArtista')
 nombreAlbum.classList.add('nombreAlbum')
 video.classList.add('video')
+
+let misCanciones = [];
+let traerCanciones = localStorage.getItem('cancionesFavoritas')
+
+if(traerCanciones != null){
+    misCanciones = JSON.parse(traerCanciones);
+}
+if(misCanciones.includes(id)){
+    playlist.innerHTML = `<a href="#"> Quitar de mi playlist </a>`
+}
+playlist.addEventListener('click', function(){
+    if(misCanciones.includes(id)){
+        let posicionCancion = misCanciones.indexOf(id);
+        misCanciones.splice(posicionCancion,1);
+        playlist.innerHTML = `<a href="#"> Agregar a mi playlist </a>`;
+    }else{
+        misCanciones.push(id);
+        playlist.innerHTML = `<a href="#"> Quitar de mi playlist </a>`;
+    }
+    let cadenaTexto = JSON.stringify(misCanciones);
+    localStorage.setItem('cancionesFavoritas',cadenaTexto);
+    console.log(localStorage);
+})
