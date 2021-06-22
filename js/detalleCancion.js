aeead91f54c28084ae894fc7b35a1ee19c8ce04e
abriniell <69683991+abriniell@users.noreply.github.com>
2021-06-21 21:19:01 -0300

    primer intento playlist

--- js/detalleCancion.js
+++ js/detalleCancion.js
@@ -2,7 +2,7 @@ let fotoCancion = document.querySelector('.fotoCancion')
 let nombreCancion = document.querySelector('.nombreCancion')
 let nombreArtista = document.querySelector('.nombreArtista')
 let nombreAlbum = document.querySelector('.nombreAlbum')
-
+let playlist = document.querySelector('.playlist')
 
 let objetoId = new URLSearchParams(location.search)
 let id = objetoId.get('id')
@@ -19,9 +19,7 @@ fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${id}`)
     nombreCancion.innerHTML += `${Datacancion.title}`
     fotoCancion.innerHTML += `<article class="fotos"> <img src="${Datacancion.album.cover_big}">  </article>`
     nombreArtista.innerHTML +=`${Datacancion.artist.name}`
-    nombreAlbum.innerHTML += `${Datacancion.album.title}`
-   
-    
+    nombreAlbum.innerHTML += `${Datacancion.album.title}`  
 })
 .catch(function(error){
     console.log(error)
@@ -30,3 +28,28 @@ fotoCancion.classList.add('fotoCancion')
 nombreCancion.classList.add('nombreCancion')
 nombreArtista.classList.add('nombreArtista')
 nombreAlbum.classList.add('nombreAlbum')
+
+    playlist.innerHTML += `<a id="agregarQuitar" href="#"> Agregar a mi playlist </a>`
+
+let misCanciones = [];
+
+let traerCanciones = localStorage.getItem('cancionesFavoritas')
+if (traerCanciones != null) {
+    misCanciones = JSON.parse(traerCanciones);
+}
+if(misCanciones.includes(id)){
+    playlist.innerHTML = 'Quitar de mi playlist';
+}
+playlist.addEventListener('click', function(){
+    e.preventDefault();
+    if (misCanciones.includes(id)){
+        let posicionCancion = misCanciones.indexOf(id);
+        misCanciones.splice(posicionCancion,1)
+        playlist.innerHTML = 'Agregar a mi playlist';
+    }else{
+        misCanciones.push(id);
+        playlist.innerHTML = 'Quitar de mi playlist'
+    }
+    let cadenaTextoCancion = JSON.stringify(misCanciones);
+    localStorage.setItem('cancionesFavoritas',cadenaTextoCancion)
+})
